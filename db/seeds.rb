b96429ef384e586767e605c2d9e993dd84940a8e
Chloe Lagneaux <chloelagneaux2@gmail.com>
2022-11-18 12:57:13 +0100

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts 'Creating movies...'
+Bookmark.destroy_all
 Movie.destroy_all
 List.destroy_all
-Bookmark.destroy_all
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-List.create(name: "Heros")
-List.create(name: "Fantastic")
-List.create(name: "Thriller")
-List.create(name: "Action")
+movie1 = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+movie2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+movie3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+movie4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-Bookmark.create(comment: "Cool movie", movie_id: 1, list_id: 2)
-Bookmark.create(comment: "It was nice", movie_id: 3, list_id: 2)
-Bookmark.create(comment: "Amazing", movie_id: 2, list_id: 2)
+list1 = List.create(name: "Heros")
+list2 = List.create(name: "Fantastic")
+list3 = List.create(name: "Thriller")
+list4 = List.create(name: "Action")
+
+Bookmark.create!(comment: "Cool movie", movie_id: movie1.id, list_id: list1.id)
+Bookmark.create!(comment: "It was nice", movie_id: movie2.id, list_id: list2.id)
+Bookmark.create!(comment: "Amazing", movie_id: movie3.id, list_id: list3.id)
 
 puts 'finished'
